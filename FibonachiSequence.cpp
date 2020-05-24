411c0f369e47436f524dc81a486bc36bbdf08a89
demidko8 <demid-tomko@mail.ru>
2020-05-24 18:25:21 +0500

    Последовательность Фибоначчи

---
+++ FibonachiSequence.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int previous = 0;
+	int current = 1;
+	for(int i=0;i<n;i++)
+	{
+		cout << previous << endl;
+		current = previous + current;
+		previous = current-previous;
+	}
+	return 0;
+
+}
+
+
