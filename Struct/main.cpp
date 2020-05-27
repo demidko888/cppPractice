94f5111f0baba6fffb890254579cd591eacdb6b5
demidko8 <demid-tomko@mail.ru>
2020-05-27 23:39:21 +0500

    Сделал лабораторную по структурам

---
+++ Struct/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct Person {
+	int age;
+	string name;
+	char grade;
+};
+Person addPerson(int age, string name, char grade);
+void printPerson(Person student);
+
+
+int main()
+{
+	Person student = addPerson(20, "Vasya", 'A');
+	printPerson(student);
+	return 0;
+}
+
+void printPerson(Person student) {
+	cout << "Peroson's age = " << student.age << endl;
+	cout << "Peroson's name = " << student.name << endl;
+	cout << "Peroson's grade = " << student.grade << endl;
+}
+
+Person addPerson(int age, string name, char grade) {
+	Person tmp;
+	tmp.age = age;
+	tmp.name = name;
+	tmp.grade = grade;
+	return tmp;
+}
