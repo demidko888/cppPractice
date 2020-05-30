3a3531803c1facfaf051c945b7522435873d86cc
demidko8 <demid-tomko@mail.ru>
2020-05-30 20:56:44 +0500

    Исправил

--- MatrixB/Matrix2D.cpp
+++ MatrixB/Matrix2D.cpp
@@ -1,10 +1,9 @@
 #include "Matrix2D.h"
 
 Matrix2D::Matrix2D() :MatrixBase(DIM){
-    int number = 0;
     for (int i = 0; i < DIM; i++)
         for (int j = 0; j < DIM; j++)
-            matrix.element(i, j) = ++number;
+            matrix.element(i, j) = i*DIM+j;
 }
 
 int Matrix2D::element(unsigned int i, unsigned int j) const{
--- MatrixB/Matrix3D.cpp
+++ MatrixB/Matrix3D.cpp
@@ -1,10 +1,9 @@
 #include "Matrix3D.h"
 
 Matrix3D::Matrix3D() :MatrixBase(DIM) {
-		int number = 0;
 		for (int i = 0; i < DIM; i++)
 			for (int j = 0; j < DIM; j++)
-				matrix.element(i, j) = ++number;
+				matrix.element(i, j) = i*DIM+j;
 	}
 
 int Matrix3D::element(unsigned int i, unsigned int j) const{
